0c12293829b2b5cb1a5dd3a03f2e0ab76f030628
airseneo <temfack2arsene@gmail.com>
2022-02-14 22:31:50 -0600

    updated

---
+++ srcs/cis/cheatsheet.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+#
+# SERVER
+#	
+#	./Assessor-CLI.sh -i -rd /var/www/html/ -nts -rp index
+#
+# Install:
+#____________________________________________________________________________
+# CLIENT
+#
+# Install:
