64755539121f1770f40a28b5c1dc0f5f8933b527
airseneo <temfack2arsene@gmail.com>
2023-12-23 23:34:38 -0600

    update

---
+++ srcs/ansible/cheatsheet.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+#
+# SERVER
+#
+# Install:
+#
+#
+#____________________________________________________________________________
+# CLIENT
+#   Create role (vm):
+#       ansible-galaxy role init vm
+#
+#
+#
+# Install:
+#_____________________________________________________________________________
+#
+#	Reference(s):
+#       https://docs.ansible.com/ansible/latest/galaxy/dev_guide.html#creating-collections-for-galaxy
---
+++ srcs/libvirt/cheatsheet.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+#
+# SERVER
+#
+# Install:
+# 	yum install virt-manager (Fedora)
+#	apt-get install virt-manager (Debian)
+#	emerge virt-manager (Gentoo)
+# 	pkg_add virt-manager (OpenBSD)
+#
+# 	systemctl start libvirtd.service
+#____________________________________________________________________________
+# CLIENT
+#   virt-manager
+#
+# Install:
+#_____________________________________________________________________________
+#
+#	Reference(s):
+#		https://virt-manager.org/
