1cbc7c8c270fd5cdd94e053640447a630506838b
airseneo <temfack2arsene@gmail.com>
2025-05-15 22:27:42 -0500

    update

--- ansible/cheatsheet.sh
+++ ansible/cheatsheet.sh
@@ -12,6 +12,16 @@
 #       ansible-galaxy role import airseneo ansible-role-vm --token <token> -v
 #       ansible-galaxy role delete --token <token> -v airseneo ansible-role-vm
 #
+#   Upgrade role (vm):
+#       git tag 2.0.0
+#       git push origin --tags
+#       ansible-galaxy role import --token <token> -v airseneo ansible-role-vm
+
+#   Create collection (vm):
+#       ansible-galaxy collection init vm
+#       ansible-galaxy collection import airseneo ansible-role-vm --token <token> -v
+#       ansible-galaxy collection delete --token <token> -v airseneo ansible-role-vm
+#
 #
 # Install:
 #_____________________________________________________________________________
